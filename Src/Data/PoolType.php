850f556f63949350c37bb735793905caa3fba9e7
Shesh Ghimire <sheshgh@outlook.com>
2024-06-07 19:08:11 +0545

    NEW: PoolType config validation & array conversion methods.

--- Src/Data/PoolType.php
+++ Src/Data/PoolType.php
@@ -9,6 +9,7 @@ declare( strict_types = 1 );
 
 namespace TheWebSolver\Codegarage\Lib\Cache\Data;
 
+use InvalidArgumentException;
 use Symfony\Component\Cache\Adapter\PdoAdapter;
 use Symfony\Component\Cache\Adapter\TagAwareAdapter;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
@@ -39,12 +40,36 @@ enum PoolType: string {
 		};
 
 		$config  = array_values( (array) $dto );
-		$args    = array( ...$config, new TagAwareMarshaller() );
+		$args    = array( ...$this->toArray( $dto ), new TagAwareMarshaller() );
 		$default = $this->adapter();
 		$adapter = TagAwareAdapter::class === $tagAware
 			? new TagAwareAdapter( new $default( ...$args ) )
 			: new $tagAware( ...$args );
 
 		return array( $adapter, $config );
+	}
+
+	public function toArray( object $config ): array {
+		$this->validateConfig( $config );
+
+		return match ( $this ) {
+			self::FileSystem => array( $config->namespace, $config->life, $config->location ),
+			self::Database   => array( $config->dsn, $config->namespace, $config->life, $config->options )
+		};
+	}
+
+	private function validateConfig( object $config ): void {
+		$class = match ( $this ) {
+			self::FileSystem => Directory::class,
+			self::Database   => PdoDsn::class,
+		};
+
+		if ( is_a( $config, $class ) ) {
+			return;
+		}
+
+		throw new InvalidArgumentException(
+			$this->fqcn() . ' only accepts configuration object of class "' . $class . '".'
+		);
 	}
 }
